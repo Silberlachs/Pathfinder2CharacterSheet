2997e559bf83eaeedd2805e4916fc1f1d499a115
Silberlachs <LuckbertGG1911>
2021-06-25 02:00:45 +0200

    Renders Skill List
    Adds CSS and Javascript callbacks

---
+++ wp-content/plugins/pathfinder/src/Renderer/SkillRenderer/SkillRenderer.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Pathfinder\Renderer\SkillRenderer;
+
+class SkillRenderer
+{
+
+    private array $skillList;
+    private string $skillTable;
+
+    public function __construct(array $skills)
+    {
+        $this->skillList = $skills;
+        $this->skillTable = '';
+    }
+
+    public function buildSkillTable():void
+    {
+        $outputString = $this->prepareTable();
+
+        foreach ($this->skillList as $skill)
+        {
+            $actions = $skill->getActions();
+            $outputString .= $this->addSkillNamesAndAbilites($skill);
+            $outputString .= $this->addUntrainedActions($actions['untrained']);
+            $outputString .= $this->addTrainedActions($actions['trained']);
+            $outputString .= $this->addProficiencyAndRollbutton($skill);
+        }
+
+        $this->skillTable = $outputString;
+    }
+
+    public function getSkillTable():string
+    {
+        return $this->skillTable;
+    }
+
+    private function prepareTable(): string
+    {
+        $tableHead = '<div class="skill">';
+        $tableHead .= '<div class="skillName skill_heading">Skill Name</div>';
+        $tableHead .= '<div class="skill_untrained skill_heading">Untrained</div>';
+        $tableHead .= '<div class="skill_trained skill_heading">Trained</div>';
+        $tableHead .= '</div>';
+        return $tableHead;
+    }
+
+    private function addSkillNamesAndAbilites(mixed $skill): string
+    {
+        $skillNamesAndAbilities = '<div class="skill" id="' . $skill->getName() . '">';
+        $skillNamesAndAbilities .= '<div class="skillName">';
+        $skillNamesAndAbilities .= $skill->getName() . '<br>( ' . $skill->getKeyAbility() . ' )';
+        $skillNamesAndAbilities .= '</div>';
+        return $skillNamesAndAbilities;
+    }
+
+    private function addUntrainedActions($untrained): string
+    {
+        $untrainedSkillList = '<div class="skill_untrained">';
+        foreach ($untrained as $untrainedAction) {
+            $untrainedSkillList .= $untrainedAction . '<br>';
+        }
+        $untrainedSkillList .= '</div>';
+        return $untrainedSkillList;
+    }
+
+    private function addTrainedActions($trained): string
+    {
+        $trainedSkillList = '<div class="skill_trained">';
+        foreach ($trained as $trainedAction) {
+            $trainedSkillList .= $trainedAction . '<br>';
+        }
+        $trainedSkillList .= '</div>';
+        return $trainedSkillList;
+    }
+
+    private function addProficiencyAndRollbutton(mixed $skill): string
+    {
+        $proficiencyAndButton = '<div class="skill_roll">';
+        $proficiencyAndButton .= '(' . $skill->getProficiency()->getProficiencyGrade() . ')';
+        $proficiencyAndButton .= '<div class="skill_roll_button">roll</div>';
+        $proficiencyAndButton .= '</div>';
+        $proficiencyAndButton .= '</div>';
+        return $proficiencyAndButton;
+    }
+}
--- wp-content/plugins/pathfinder/src/Renderer/TemplateLoader.php
+++ wp-content/plugins/pathfinder/src/Renderer/TemplateLoader.php
@@ -2,6 +2,8 @@
 
 namespace Pathfinder\Renderer;
 
+use Pathfinder\Renderer\SkillRenderer\SkillRenderer;
+
 class TemplateLoader
 {
     private string $template;
@@ -24,78 +26,80 @@ class TemplateLoader
 
     public function replaceAbilityScores(array $abilityScores):void
     {
-        $this->template = str_replace('{{strength_score}}', $abilityScores['strength']->getScore(), $this->template);
-        $this->template = str_replace(
-            '{{strength_modifier}}',
-            -5 + round(($abilityScores['strength']->getScore()/2),
-                PHP_ROUND_HALF_DOWN), $this->template
-        );
-
-        $this->template = str_replace('{{dexterity_score}}', $abilityScores['dexterity']->getScore(), $this->template);
-        $this->template = str_replace(
-            '{{dexterity_modifier}}',
-            -5 + round(($abilityScores['dexterity']->getScore()/2),
-                PHP_ROUND_HALF_DOWN), $this->template
-        );
-
-        $this->template = str_replace('{{constitution_score}}', $abilityScores['constitution']->getScore(), $this->template);
-        $this->template = str_replace(
-            '{{constitution_modifier}}',
-            -5 + round(($abilityScores['constitution']->getScore()/2),
-                PHP_ROUND_HALF_DOWN), $this->template
-        );
-
-        $this->template = str_replace('{{intelligence_score}}', $abilityScores['intelligence']->getScore(), $this->template);
-        $this->template = str_replace(
-            '{{intelligence_modifier}}',
-            -5 + round(($abilityScores['intelligence']->getScore()/2),
-                PHP_ROUND_HALF_DOWN), $this->template
-        );
-
-        $this->template = str_replace('{{wisdom_score}}', $abilityScores['wisdom']->getScore(), $this->template);
-        $this->template = str_replace(
-            '{{wisdom_modifier}}',
-            -5 + round(($abilityScores['wisdom']->getScore()/2),
-                PHP_ROUND_HALF_DOWN), $this->template
-        );
-
-        $this->template = str_replace('{{charisma_score}}', $abilityScores['charisma']->getScore(), $this->template);
-        $this->template = str_replace(
-            '{{charisma_modifier}}',
-            -5 + round(($abilityScores['charisma']->getScore()/2),
-                PHP_ROUND_HALF_DOWN), $this->template
-        );
+        $this->replaceStrength($abilityScores['strength']);
+        $this->replaceDexterity($abilityScores['dexterity']);
+        $this->replaceConstitution($abilityScores['constitution']);
+        $this->replaceIntelligence($abilityScores['intelligence']);
+        $this->replaceWisdom($abilityScores['wisdom']);
+        $this->replaceCharisma($abilityScores['charisma']);
     }
 
     public function replaceSkillList(array $skills)
     {
-        $outputString = '';
+        $skillRenderer = new SkillRenderer($skills);
+        $skillRenderer->buildSkillTable();
 
-        foreach ($skills as $skill)
-        {
-            $outputString .= '<div>';
-            $outputString .= $skill->getName() . '  ( '.$skill->getKeyAbility().' )<br>';
-            $outputString .= '( ' . $skill->getProficiency()->getProficiencyGrade() . ' )<br>';
+        $this->template = str_replace('{{Skills}}', $skillRenderer->getSkillTable(), $this->template);
+    }
 
-            $actions = $skill->getActions();
+    private function replaceStrength($strength): void
+    {
+        $this->template = str_replace('{{strength_score}}', $strength->getScore(), $this->template);
+        $this->template = str_replace(
+            '{{strength_modifier}}',
+            -5 + round(($strength->getScore() / 2),
+                PHP_ROUND_HALF_DOWN), $this->template
+        );
+    }
 
-            $outputString .= 'Untrained Actions:<br>';
-            foreach ($actions['untrained'] as $untrainedActions)
-            {
-                $outputString .= $untrainedActions . '<br>';
-            }
+    private function replaceDexterity($dexterity): void
+    {
+        $this->template = str_replace('{{dexterity_score}}', $dexterity->getScore(), $this->template);
+        $this->template = str_replace(
+            '{{dexterity_modifier}}',
+            -5 + round(($dexterity->getScore() / 2),
+                PHP_ROUND_HALF_DOWN), $this->template
+        );
+    }
 
-            $outputString .= 'Trained Actions:<br>';
-            foreach ($actions['trained'] as $trainedActions)
-            {
-                $outputString .= $trainedActions . '<br>';
-            }
+    private function replaceConstitution($constitution): void
+    {
+        $this->template = str_replace('{{constitution_score}}', $constitution->getScore(), $this->template);
+        $this->template = str_replace(
+            '{{constitution_modifier}}',
+            -5 + round(($constitution->getScore() / 2),
+                PHP_ROUND_HALF_DOWN), $this->template
+        );
+    }
 
-            $outputString .= '<br></div>';
-        }
+    private function replaceIntelligence($intelligence): void
+    {
+        $this->template = str_replace('{{intelligence_score}}', $intelligence->getScore(), $this->template);
+        $this->template = str_replace(
+            '{{intelligence_modifier}}',
+            -5 + round(($intelligence->getScore() / 2),
+                PHP_ROUND_HALF_DOWN), $this->template
+        );
+    }
 
-        $this->template = str_replace('{{Skills}}', $outputString, $this->template);
+    private function replaceWisdom($wisdom): void
+    {
+        $this->template = str_replace('{{wisdom_score}}', $wisdom->getScore(), $this->template);
+        $this->template = str_replace(
+            '{{wisdom_modifier}}',
+            -5 + round(($wisdom->getScore() / 2),
+                PHP_ROUND_HALF_DOWN), $this->template
+        );
+    }
 
+    private function replaceCharisma($charisma): void
+    {
+        $this->template = str_replace('{{charisma_score}}', $charisma->getScore(), $this->template);
+        $this->template = str_replace(
+            '{{charisma_modifier}}',
+            -5 + round(($charisma->getScore() / 2),
+                PHP_ROUND_HALF_DOWN), $this->template
+        );
     }
 
 }
